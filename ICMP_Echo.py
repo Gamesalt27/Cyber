74a6fb8a811c1db4a1d44927a66f62d3566fa1a1
Ofek <ofek.c100@gmail.com>
2023-01-13 17:52:14 +0200

    spoofed ip in icmp

--- ICMP_Echo.py
+++ ICMP_Echo.py
@@ -1,5 +1,13 @@
 from scapy.all import *
-from scapy.layers.dns import DNS, DNSQR, DNSRR
-from scapy.layers.inet import IP, UDP
+from scapy.layers.inet import IP, UDP, ICMP
 from scapy.layers.l2 import Ether
+import logging
 
+
+def main():
+    packet = IP(dst="142.251.142.206", src="10.100.102.7")/ICMP()
+    recieved = sr1(packet)
+    recieved.show()
+
+if __name__ == "__main__":
+    main()
